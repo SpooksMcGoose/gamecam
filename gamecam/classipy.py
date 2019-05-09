11b7cb0915ca839537069ef4b071e44e7a4bdf54
Shane Drabing <shane.drabing@gmail.com>
2019-05-08 19:06:14 -0700

    Messing around with auto-sizing images

--- gamecam/classipy.py
+++ gamecam/classipy.py
@@ -55,19 +55,12 @@ class Navbar(tk.Frame):
         self.combo = ttk.Combobox(self.parent)
         self.combo['values']= (240, 480, 600, 768, 1024)
         self.combo.current(0)
-        self.combo.pack(side="top")
+        #self.combo.pack(side="bottom")
 
-        self.combo.bind("<<ComboboxSelected>>", self.combo_updated)
+        #self.combo.bind("<<ComboboxSelected>>", self.combo_updated)
 
-    def combo_updated(self, *args):
+    def combo_updated(self, event):
         self.parent.main.update()
-
-    def get_dir(self):
-        output = filedialog.askopenfilenames(
-            initialdir=self.HOME_DIR ,
-            title="Choose files to view",
-        )
-        return output
 
 
 class Main(tk.Frame):
@@ -82,13 +75,16 @@ class Main(tk.Frame):
         self.image_paths = sorted(self.parent.navbar.get_dir())
         self.update()
 
+        self.b = tk.Button(self.parent, text="OK", command=self.update)
+        self.b.pack()
+
     def update(self):
         self.index = min(len(self.image_paths) - 1, max(0, self.index))
-        combo_val = self.parent.navbar.combo.get()
-
+        max_size = max(100, self.winfo_width() // 2)
+            
         im_paths = (self.image_paths[self.index+i] for i in range(2))
         im_pil = (Image.open(fp) for fp in im_paths)
-        im_resize = (pil_resize(combo_val, im) for im in im_pil)
+        im_resize = (pil_resize(max_size, im) for im in im_pil)
         im_tk = [pil_to_tkinter(im) for im in im_resize]
 
         im1, im2 = im_tk
@@ -97,8 +93,8 @@ class Main(tk.Frame):
             self.panel2 = tk.Label(image=im2)
             self.panel1.image = im1
             self.panel2.image = im2
-            self.panel1.pack(side="right", padx=10, pady=10)
-            self.panel2.pack(side="right", padx=10, pady=10)
+            self.panel1.pack(side="left")
+            self.panel2.pack(side="right")
         else:
             self.panel1.configure(image=im1)
             self.panel2.configure(image=im2)
@@ -109,11 +105,54 @@ class Main(tk.Frame):
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.navbar = Navbar(self)
-        self.main = Main(self)
+        self.parent = parent
 
-        self.navbar.pack(side="bottom", fill="y", expand=True)
-        self.main.pack(side="top", fill="y")
+        self.HOME_DIR = os.path.expanduser("~")
+
+        self.panel1 = None
+        self.panel2 = None
+        self.index = 0
+
+        self.image_paths = sorted(self.get_dir())
+        self.update()
+
+        self.b = tk.Button(self.parent, text="OK", command=self.update)
+        self.b.pack(side="bottom")
+
+    def get_dir(self):
+        output = filedialog.askopenfilenames(
+            initialdir=self.HOME_DIR ,
+            title="Choose files to view",
+        )
+        return output
+
+    def update(self):
+        self.index = min(len(self.image_paths) - 1, max(0, self.index))
+        print(self.winfo_width())
+        print(self.winfo_reqwidth())
+        print()
+        print(self.parent.winfo_width())
+        print(self.parent.winfo_reqwidth())
+        max_size = max(100, self.parent.winfo_width() // 2)
+
+        im_paths = (self.image_paths[self.index+i] for i in range(2))
+        im_pil = (Image.open(fp) for fp in im_paths)
+        im_resize = (pil_resize(max_size, im) for im in im_pil)
+        im_tk = [pil_to_tkinter(im) for im in im_resize]
+
+        im1, im2 = im_tk
+        if self.panel1 is None or self.panel2 is None:
+            self.panel1 = tk.Label(image=im1)
+            self.panel2 = tk.Label(image=im2)
+            self.panel1.image = im1
+            self.panel2.image = im2
+            self.panel1.pack(side="top")
+            self.panel2.pack(side="top")
+        else:
+            self.panel1.configure(image=im1)
+            self.panel2.configure(image=im2)
+            self.panel1.image = im1
+            self.panel2.image = im2
 
 
 if __name__ == "__main__":
