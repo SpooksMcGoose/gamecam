ca53cd260abeae5570bfaf5472eec68c547f0568
Shane Drabing <shane.drabing@gmail.com>
2019-05-08 18:23:31 -0700

    Started working on classipy.py

--- gamecam/classipy.py
+++ gamecam/classipy.py
@@ -1,4 +1,120 @@
-from gamecam import handle_tkinter, find_imgs
+import os
+import cv2
+from PIL import ImageTk
+from PIL import Image
+
+try:
+    import tkinter as tk
+    from tkinter import ttk
+    from tkinter import filedialog
+except ImportError:
+    import Tkinter as tk
+    from Tkinter import ttk
+    from Tkinter import filedialog
+
+
+# GENERAL FUNCTIONS
+
+
+def initialize_root():
+    root = tk.Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
+
+
+# IMAGE FUNCTIONS
+
+
+def opencv_to_tkinter(cv_im):
+    cv_im = cv2.cvtColor(cv_im, cv2.COLOR_BGR2RGB)
+    pil_im = Image.fromarray(cv_im)
+    return pil_to_tkinter(pil_im)
+
+
+def pil_to_tkinter(pil_im):
+    return ImageTk.PhotoImage(pil_im)
+
+
+def pil_resize(max_size, pil_im):
+    max_size = int(max_size)
+    w, h = pil_im.size
+    scale = min(max_size / w, max_size / h)
+    return pil_im.resize((round(w * scale), round(h * scale)), Image.ANTIALIAS)
+
+
+# GUI CLASSES
+
+
+class Navbar(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        self.HOME_DIR = os.path.expanduser("~")
+
+        self.combo = ttk.Combobox(self.parent)
+        self.combo['values']= (240, 480, 600, 768, 1024)
+        self.combo.current(0)
+        self.combo.pack(side="top")
+
+        self.combo.bind("<<ComboboxSelected>>", self.combo_updated)
+
+    def combo_updated(self, *args):
+        self.parent.main.update()
+
+    def get_dir(self):
+        output = filedialog.askopenfilenames(
+            initialdir=self.HOME_DIR ,
+            title="Choose files to view",
+        )
+        return output
+
+
+class Main(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+        self.panel1 = None
+        self.panel2 = None
+        self.index = 0
+
+        self.image_paths = sorted(self.parent.navbar.get_dir())
+        self.update()
+
+    def update(self):
+        self.index = min(len(self.image_paths) - 1, max(0, self.index))
+        combo_val = self.parent.navbar.combo.get()
+
+        im_paths = (self.image_paths[self.index+i] for i in range(2))
+        im_pil = (Image.open(fp) for fp in im_paths)
+        im_resize = (pil_resize(combo_val, im) for im in im_pil)
+        im_tk = [pil_to_tkinter(im) for im in im_resize]
+
+        im1, im2 = im_tk
+        if self.panel1 is None or self.panel2 is None:
+            self.panel1 = tk.Label(image=im1)
+            self.panel2 = tk.Label(image=im2)
+            self.panel1.image = im1
+            self.panel2.image = im2
+            self.panel1.pack(side="right", padx=10, pady=10)
+            self.panel2.pack(side="right", padx=10, pady=10)
+        else:
+            self.panel1.configure(image=im1)
+            self.panel2.configure(image=im2)
+            self.panel1.image = im1
+            self.panel2.image = im2
+
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.navbar = Navbar(self)
+        self.main = Main(self)
+
+        self.navbar.pack(side="bottom", fill="y", expand=True)
+        self.main.pack(side="top", fill="y")
+
 
 if __name__ == "__main__":
-    find_imgs()
+    initialize_root()
--- gamecam/pyrcolate.py
+++ gamecam/pyrcolate.py
@@ -9,11 +9,22 @@ import cv2
 import exifread as er
 import numpy as np
 
-from gamecam import handle_tkinter, find_imgs
 from datetime import datetime as dt, timedelta as td
 
 from matplotlib import pyplot as plt
 from matplotlib.widgets import Slider
+
+try:
+    import tkinter as tk
+    from tkinter import filedialog
+except ImportError:
+    import Tkinter as tk
+    from Tkinter import filedialog
+
+# Work-around for a weird tkinter / matplotlib interaction.
+# Note: I would use TkAgg backend, but key bouncing breaks the plot.
+root = tk.Tk()
+root.withdraw()
 
 
 # CONSTANTS
@@ -67,6 +78,54 @@ DIRECTION_MULTS = {
 # GENERAL FUNCTIONS
 
 
+def handle_tkinter(mode, init=None):
+    """Used for making basic Tkinter dialog calls.
+
+    For some reason, Tkinter doesn"t like to be used within a class,
+    so this is here instead.
+
+    Parameters
+    ----------
+    mode : {"savefile", "openfile", "opendir"}
+        Type of file dialog to call.
+    init : str, optional
+        Starting path for file dialog.
+
+    Returns
+    -------
+    str
+        User-selected path name from file dialog.
+    """
+
+    root = tk.Tk()
+    root.withdraw()
+
+    if init is None:
+        init = os.path.expanduser("~")
+
+    if mode == "savefile":
+        output = filedialog.asksaveasfilename(
+            initialdir=init,
+            filetypes=(("Save files", "*.sav"), ("All files", "*.*"))
+        )
+    elif mode == "openfile":
+        output = filedialog.askopenfilename(
+            initialdir=init,
+            title="Select file",
+            filetypes=(("Save files", "*.sav"), ("All files", "*.*"))
+        )
+    elif mode == "opendir":
+        output = filedialog.askdirectory(
+            initialdir=init,
+            title="Select folder",
+        )
+
+    root.update()
+    root.destroy()
+
+    return output
+
+
 def to_24_hour(datetime):
     """Converts datetime.datetime type to 24 hour float type.
     """
@@ -112,6 +171,49 @@ def strfdelta(tdelta, fmt):
 
 
 # SPECIFIC FUNCTIONS (ALL BELOW)
+
+
+def find_imgs(dirpath=None, img_type=(".jpg", ".jpeg")):
+    """Walks directory path, finding all files ending in img_type.
+
+    Parameters
+    ----------
+    dirpath : str, optional
+        If no path provided, the user can navigate to it via tkinter window.
+    img_type : tuple, optional
+        By default, finds JPG image types, but can be changed if camera
+        exports a different filetype.
+
+    Returns
+    -------
+    list of dictionaries
+        Contains filenames and filepaths.
+    """
+
+    if dirpath is None:
+        dirpath = handle_tkinter("opendir")
+
+    if dirpath:
+        output = []
+
+        for dir_, subdir, files in os.walk(dirpath):
+            if "_selected" not in dir_:
+                found = (
+                    f for f in files
+                    if f.lower().endswith(img_type)
+                )
+                for filename in found:
+                    filepath = os.path.join(dir_, filename)
+
+                    output.append({
+                        "filename": filename,
+                        "filepath": filepath
+                    })
+
+        return output
+    else:
+        print("NO IMAGE DIRECTORY SPECIFIED!")
+        return []
 
 
 def attach_exif(jpg_data, parse_tags=DEFAULT_PARSE):
@@ -738,8 +840,10 @@ class Cam():
                     new_name = "_".join((dt_ISO, row["filename"]))
                     new_path = os.path.join(directory, new_name)
 
-                    write_row["new_name"] = new_name
-                    write_row["new_path"] = new_path
+                    write_row["filename"] = new_name
+                    write_row["filepath"] = new_path
+                    write_row["old_name"] = row["filename"]
+                    write_row["old_path"] = row["filepath"]
 
                     shutil.copy2(row["filepath"], new_path)
                     write_data.append(write_row)
